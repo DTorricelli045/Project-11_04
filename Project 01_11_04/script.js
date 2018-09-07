b1c8ce8b7125a44709fc6a75243911b8b13bd23d
Unknown <DTorricelli045@west-mec.org>
2018-09-07 14:59:33 -0700

    Final commit
    All code complete

--- Project 01_11_04/script.js
+++ Project 01_11_04/script.js
@@ -11,18 +11,26 @@
 
 //global variables
 var httpRequest = false;
+var countrySel
 
 //React based on user input in postal code box
 function getRequestObject() {
     try {
         httpRequest = new XMLHttpRequest();
     } catch (requestError) {
+        document.getElementById("zipset").style.visibility = "visible";
         document.getElementById("csset").style.visibility = "visible";
+        var germany = document.getElementById("germany");
+        var us = document.getElementById("us");
         var zip = document.getElementById("zip").value;
         if (zip.addEventListener) {
+            germany.removeEventListener("click", checkButtons, false);
+            us.removeEventListener("click", checkButtons, false);
             zip.removeEventListener("keyup", checkInput, false);
         }
         else if (zip.attachEvent) {
+            germany.detachEvent("onclick", checkButtons);
+            us.detachEvent("onclick", checkButtons);
             zip.detachEvent("onkeyup", checkInput);
         }
         return false;
@@ -43,6 +51,21 @@ function checkInput() {
     }
 }
 
+//function to check radio button input
+function checkButtons() {
+    var germany = document.getElementById("germany");
+    var us = document.getElementById("us");
+    if (germany.checked || us.checked) {
+        document.getElementById("zipset").style.visibility = "visible";
+        if (germany.checked) {
+            countrySel = "de";
+        }
+        else {
+            countrySel = "us";
+        }
+    }
+}
+
 //function to gather location info
 function getLocation() {
     var zip = document.getElementById("zip").value;
@@ -50,7 +73,7 @@ function getLocation() {
         httpRequest = getRequestObject();
     }
     httpRequest.abort();
-    httpRequest.open("get", "http://api.zippopotam.us/us/" + zip, true);
+    httpRequest.open("get", "http://api.zippopotam.us/" + countrySel + "/" + zip, true);
     httpRequest.send(null);
     httpRequest.onreadystatechange = displayData;
 }
@@ -62,13 +85,25 @@ function displayData() {
         var city = document.getElementById("city");
         var state = document.getElementById("state");
         city.value = resultData.places[0]["place name"];
-        state.value = resultData.places[0]["State abbreviation"];
+        state.value = resultData.places[0]["state abbreviation"];
         document.getElementById("zip").blur();
         document.getElementById("csset").style.visibility = "visible";
     }
 }
 
-//Checks for inputs in the postal code text box
+//Checks for radio button inputs
+var germany = document.getElementById("germany");
+var us = document.getElementById("us");
+if (us.addEventListener){
+    germany.addEventListener("click", checkButtons, false);
+    us.addEventListener("click", checkButtons, false);
+}
+else if (us.attachEvent) {
+    germany.attachEvent("onclick", checkButtons);
+    us.attachEvent("onclick", checkButtons);
+}
+
+//Checks for text box inputs
 var zip = document.getElementById("zip");
 if (zip.addEventListener){
     zip.addEventListener("keyup", checkInput, false);
